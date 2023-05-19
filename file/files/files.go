808f2285ff77abc911f4795a667a4a2e54c48e6f
Rehtt <dsreshiram@gmail.com>
2023-05-19 20:41:53 +0800

    :sparkles: files reader

---
+++ file/files/files.go
@@ -0,0 +1,63 @@
+package files
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+type Files struct {
+	list  []string
+	index int
+	buf   bytes.Buffer
+}
+
+func NewReader(fileList []string) *Files {
+	return &Files{
+		list: fileList,
+	}
+}
+
+func (f *Files) Read(b []byte) (n int, err error) {
+	if err = f.init(); err != nil {
+		return 0, err
+	}
+	n, err = f.buf.Read(b)
+	if err != nil {
+		if err != io.EOF {
+			return 0, err
+		}
+		if len(b) > n {
+			m, err := f.Read(b[n:])
+			if err != nil {
+				return 0, err
+			}
+			n += m
+		}
+	}
+	return
+}
+func (f *Files) init() error {
+	if f.buf.Len() == 0 {
+		if f.index >= len(f.list) {
+			return io.EOF
+		}
+		defer func() {
+			f.index += 1
+		}()
+		if f.list[f.index] == "" {
+			return f.init()
+		}
+		file, err := os.Open(f.list[f.index])
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		f.buf.Reset()
+		_, err = f.buf.ReadFrom(file)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
