91b9ecef5874b8707d0e140660c1bb465d4e8470
Rehtt <dsreshiram@gmail.com>
2023-10-11 11:40:41 +0800

    :sparkles: to string

--- strings/string2bytes.go
+++ strings/string2bytes.go
@@ -8,3 +8,9 @@ func ToBytes(s string) []byte {
 	h := [3]uintptr{x[0], x[1], x[1]}
 	return *(*[]byte)(unsafe.Pointer(&h))
 }
+
+func ToString(b []byte) string {
+	x := (*[3]uintptr)(unsafe.Pointer(&b))
+	h := [2]uintptr{x[0], x[1]}
+	return *(*string)(unsafe.Pointer(&h))
+}
--- strings/string2bytes_test.go
+++ strings/string2bytes_test.go
@@ -9,3 +9,8 @@ func TestToBytes(t *testing.T) {
 	a := "test test test"
 	fmt.Println(ToBytes(a))
 }
+
+func TestToString(t *testing.T) {
+	a := []byte{'t', 'e', 's', 't'}
+	fmt.Println(ToString(a))
+}
