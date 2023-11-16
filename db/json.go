3a0898d9c1fc6266cbb957c5d7467328bdd1ee6d
Rehtt <dsreshiram@gmail.com>
2023-11-16 17:40:19 +0800

    :boom: json type

--- db/json.go
+++ db/json.go
@@ -3,29 +3,30 @@ package db
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"errors"
-	"fmt"
 )
 
 // json类型
+type JSON[T any] struct {
+	data T
+}
 
-type JSON json.RawMessage
-
-func (JSON) GormDataType() string {
+func (JSON[T]) GormDataType() string {
 	return "json"
 }
 
-func (j *JSON) Scan(value any) error {
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
-	}
-	j1 := json.RawMessage{}
-	err := json.Unmarshal(bytes, &j1)
-	*j = JSON(j1)
-	return err
+func (j *JSON[T]) Scan(value any) error {
+	return json.Unmarshal(value.([]byte), &j.data)
 }
 
-func (j JSON) Value() (value driver.Value, err error) {
-	return json.RawMessage(j).MarshalJSON()
+func (j JSON[T]) Value() (value driver.Value, err error) {
+	src, err := json.Marshal(j.data)
+	return src, err
+}
+
+func (j JSON[T]) Get() T {
+	return j.data
+}
+
+func (j *JSON[T]) Set(data T) {
+	j.data = data
 }
