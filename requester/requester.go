19edb40e70f3446fb9935caa37f6d6d7e6864341
Rehtt <dsreshiram@gmail.com>
2024-09-14 08:03:56 +0000

    :art: :ambulance:

--- requester/requester.go
+++ requester/requester.go
@@ -2,9 +2,10 @@ package requester
 
 import (
 	"context"
-	"github.com/Rehtt/Kit/bytes"
 	"io"
 	"net/http"
+
+	"github.com/Rehtt/Kit/bytes"
 )
 
 type Requester struct {
@@ -19,33 +20,44 @@ func NewRequester() *Requester {
 		header: map[string][]string{},
 	}
 }
+
 func (h *Requester) Get(u string) *Requester {
 	h.url = u
 	h.m = http.MethodGet
 	return h
 }
+
 func (h *Requester) Post(u string, body io.Reader) *Requester {
 	h.m = http.MethodPost
 	h.url = u
 	h.body = body
 	return h
 }
+
 func (h *Requester) Put(u string, body io.Reader) *Requester {
 	h.m = http.MethodPut
 	h.url = u
 	h.body = body
 	return h
 }
+
 func (h *Requester) Delete(u string, body io.Reader) *Requester {
 	h.m = http.MethodDelete
 	h.url = u
 	h.body = body
 	return h
 }
-func (h *Requester) Head(key, value string) *Requester {
+
+func (h *Requester) AddHead(key, value string) *Requester {
 	h.header.Add(key, value)
 	return h
 }
+
+func (h *Requester) SetHead(key, value string) *Requester {
+	h.header.Set(key, value)
+	return h
+}
+
 func (h *Requester) Response(ctx context.Context) (*http.Response, error) {
 	req, err := http.NewRequestWithContext(ctx, h.m, h.url, h.body)
 	if err != nil {
@@ -54,6 +66,7 @@ func (h *Requester) Response(ctx context.Context) (*http.Response, error) {
 	req.Header = h.header.Clone()
 	return http.DefaultClient.Do(req)
 }
+
 func (h *Requester) AsBytes(ctx context.Context) []byte {
 	resp, err := h.Response(ctx)
 	if err != nil {
@@ -63,6 +76,7 @@ func (h *Requester) AsBytes(ctx context.Context) []byte {
 	data, _ := io.ReadAll(resp.Body)
 	return data
 }
+
 func (h *Requester) AsString(ctx context.Context) string {
 	return bytes.ToString(h.AsBytes(ctx))
 }
