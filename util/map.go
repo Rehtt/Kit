97ccd6a120fbc987035c94a9995d6f33b2a96845
Rehtt <dsreshiram@gmail.com>
2025-01-22 17:56:42 +0800

    :zap: 移除低性能map实现

--- util/map.go
+++ util/map.go
@@ -1,76 +1,69 @@
 package util
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"hash/fnv"
 	"sync"
 )
 
-type MapItem struct {
-	m  map[string]any
-	mu sync.RWMutex
+var SHARD_COUNT uint32 = 32
+
+// 分成SHARD_COUNT个分片的map
+type ConcurrentMap[T any] []*ConcurrentMapShared[T]
+
+// 通过RWMutex保护的线程安全的分片，包含一个map
+type ConcurrentMapShared[T any] struct {
+	items map[string]T
+	sync.RWMutex
 }
 
-type Maps map[uint8]*MapItem
-
-func NewMap() Maps {
-	return make(Maps)
-}
-
-func (m Maps) Get(key string, f ...func(any, bool)) (any, bool) {
-	k := m.hash(key)
-	mm, ok := m[k[0]]
-	if !ok {
-		for _, ff := range f {
-			ff(nil, false)
-		}
-		return nil, false
+// 创建并发map
+func NewConcurrentMap[T any]() ConcurrentMap[T] {
+	m := make(ConcurrentMap[T], SHARD_COUNT)
+	for i := uint32(0); i < SHARD_COUNT; i++ {
+		m[i] = &ConcurrentMapShared[T]{items: make(map[string]T)}
 	}
-	mm.mu.RLock()
-	data, ok := mm.m[k]
-	for _, ff := range f {
-		ff(data, ok)
-	}
-
-	mm.mu.RUnlock()
-	return data, ok
+	return m
 }
 
-func (m Maps) Set(key string, value any, f ...func()) {
-	k := m.hash(key)
-	mm := m[k[0]]
-	if mm == nil {
-		mm = &MapItem{
-			m: map[string]any{},
-		}
-		m[k[0]] = mm
-	}
-	mm.mu.Lock()
-	m.runFunc(f)
-	mm.m[k] = value
-	mm.mu.Unlock()
+// 根据key计算分片索引
+func (m ConcurrentMap[T]) GetShard(key string) *ConcurrentMapShared[T] {
+	f := fnv.New32a()
+	f.Write([]byte(key))
+
+	return m[f.Sum32()%SHARD_COUNT]
 }
 
-func (m Maps) Delete(key string, f ...func()) {
-	k := m.hash(key)
-	mm := m[k[0]]
-	if mm == nil {
-		return
-	}
-	mm.mu.Lock()
-	m.runFunc(f)
-	delete(mm.m, k)
-	mm.mu.Unlock()
+func (m ConcurrentMap[T]) Set(key string, value T) {
+	// 根据key计算出对应的分片
+	shard := m.GetShard(key)
+	shard.Lock()
+	defer shard.Unlock()
+	shard.items[key] = value
 }
 
-func (m Maps) runFunc(funcs []func()) {
-	for _, f := range funcs {
-		f()
-	}
+func (m ConcurrentMap[T]) SetByFunc(key string, newValueFunc func(oldValue T) (newValue T)) (newValue T) {
+	shard := m.GetShard(key)
+	shard.Lock()
+	defer shard.Unlock()
+	oldValue := shard.items[key]
+	newValue = newValueFunc(oldValue)
+	shard.items[key] = newValue
+	return
 }
 
-func (m Maps) hash(key string) string {
-	s := sha256.New()
-	s.Write([]byte(key))
-	return hex.EncodeToString(s.Sum(nil))
+func (m ConcurrentMap[T]) Get(key string) (T, bool) {
+	// 根据key计算出对应的分片
+	shard := m.GetShard(key)
+	shard.RLock()
+	defer shard.RUnlock()
+	val, ok := shard.items[key]
+	return val, ok
+}
+
+func (m ConcurrentMap[T]) Delete(key string) {
+	// 根据key计算出对应的分片
+	shard := m.GetShard(key)
+	shard.Lock()
+	defer shard.Unlock()
+	delete(shard.items, key)
 }
--- util/map_test.go
+++ util/map_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestMap(t *testing.T) {
-	m := NewMap()
-	m.Set("123", 1234)
-	m.Set("234", 4556)
+	m := NewConcurrentMap[string]()
+	m.Set("123", "1234")
+	m.Set("234", "234")
 	fmt.Println(m.Get("123"))
 	fmt.Println(m.Get("234"))
 	fmt.Println(m.Get("asdf"))
@@ -21,7 +21,7 @@ func TestMap(t *testing.T) {
 
 func TestMapB(t *testing.T) {
 	start := time.Now()
-	m := NewMap()
+	m := NewConcurrentMap[int]()
 	for i := 0; i < 9999999; i++ {
 		m.Set(strconv.Itoa(i), i)
 		if i%100000 == 0 {
