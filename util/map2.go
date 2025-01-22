594704b4b799e1b9c3ce152a30a871d06a977608
Rehtt <dsreshiram@gmail.com>
2025-01-22 17:48:49 +0800

    :sparkles: 并发map使用泛型

--- util/map2.go
+++ util/map2.go
@@ -8,32 +8,32 @@ import (
 var SHARD_COUNT uint32 = 32
 
 // 分成SHARD_COUNT个分片的map
-type ConcurrentMap []*ConcurrentMapShared
+type ConcurrentMap[T any] []*ConcurrentMapShared[T]
 
 // 通过RWMutex保护的线程安全的分片，包含一个map
-type ConcurrentMapShared struct {
-	items map[string]any
+type ConcurrentMapShared[T any] struct {
+	items map[string]T
 	sync.RWMutex
 }
 
 // 创建并发map
-func NewConcurrentMap() ConcurrentMap {
-	m := make(ConcurrentMap, SHARD_COUNT)
+func NewConcurrentMap[T any]() ConcurrentMap[T] {
+	m := make(ConcurrentMap[T], SHARD_COUNT)
 	for i := uint32(0); i < SHARD_COUNT; i++ {
-		m[i] = &ConcurrentMapShared{items: make(map[string]any)}
+		m[i] = &ConcurrentMapShared[T]{items: make(map[string]T)}
 	}
 	return m
 }
 
 // 根据key计算分片索引
-func (m ConcurrentMap) GetShard(key string) *ConcurrentMapShared {
+func (m ConcurrentMap[T]) GetShard(key string) *ConcurrentMapShared[T] {
 	f := fnv.New32a()
 	f.Write([]byte(key))
 
 	return m[f.Sum32()%SHARD_COUNT]
 }
 
-func (m ConcurrentMap) Set(key string, value any) {
+func (m ConcurrentMap[T]) Set(key string, value T) {
 	// 根据key计算出对应的分片
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -41,7 +41,7 @@ func (m ConcurrentMap) Set(key string, value any) {
 	shard.items[key] = value
 }
 
-func (m ConcurrentMap) SetByFunc(key string, newValueFunc func(oldValue any) (newValue any)) (newValue any) {
+func (m ConcurrentMap[T]) SetByFunc(key string, newValueFunc func(oldValue T) (newValue T)) (newValue T) {
 	shard := m.GetShard(key)
 	shard.Lock()
 	defer shard.Unlock()
@@ -51,11 +51,19 @@ func (m ConcurrentMap) SetByFunc(key string, newValueFunc func(oldValue any) (ne
 	return
 }
 
-func (m ConcurrentMap) Get(key string) (any, bool) {
+func (m ConcurrentMap[T]) Get(key string) (T, bool) {
 	// 根据key计算出对应的分片
 	shard := m.GetShard(key)
 	shard.RLock()
 	defer shard.RUnlock()
 	val, ok := shard.items[key]
 	return val, ok
+}
+
+func (m ConcurrentMap[T]) Delete(key string) {
+	// 根据key计算出对应的分片
+	shard := m.GetShard(key)
+	shard.Lock()
+	defer shard.Unlock()
+	delete(shard.items, key)
 }
