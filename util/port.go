3ed739fe66a6f86e686c894e2f0ce1b9604b5591
Rehtt <dsreshiram@gmail.com>
2025-01-26 09:16:24 +0000

    :art: port

--- util/port.go
+++ util/port.go
@@ -8,9 +8,6 @@ const MaxPort uint16 = 1<<16 - 1
 
 // 返回随机端口
 func RandomPort(start, end uint16) uint16 {
-	if end > MaxPort {
-		return 0
-	}
 	port := random.RandInt64(int64(start), int64(end))
 	return uint16(port)
 }
