363590566769a7194d283349820a35905a58b2e6
Rehtt <dsreshiram@gmail.com>
2025-01-27 10:21:32 +0800

    :recycle: 调整 OpenBrowser

--- browser/open_windows.go
+++ browser/open_windows.go
@@ -1,10 +1,10 @@
-//go:build !windows
+//go:build windows
 
 package browser
 
 import (
 	"os/exec"
-	"runtime"
+	"syscall"
 )
 
 // OpenBrowser 开启浏览器
@@ -12,13 +12,10 @@ func OpenBrowser(url string) error {
 	var cmd string
 	var args []string
 
-	switch runtime.GOOS {
-	case "darwin":
-		cmd = "open"
-	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
-	}
+	cmd = "cmd"
+	args = []string{"/c", "start"}
 	args = append(args, url)
 	c := exec.Command(cmd, args...)
+	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return c.Start()
 }
