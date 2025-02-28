4e49f860aec27a54a2f836b0361c740e87b945bd
Rehtt <dsreshiram@gmail.com>
2025-02-28 15:51:42 +0800

    :art: requester

--- requester/requester.go
+++ requester/requester.go
@@ -2,8 +2,10 @@ package requester
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"net/http"
+	"sync"
 
 	"github.com/Rehtt/Kit/bytes"
 )
@@ -13,17 +15,36 @@ type Requester struct {
 	m      string
 	header http.Header
 	body   io.Reader
+
+	err error
+}
+
+var requesterPool = sync.Pool{
+	New: func() any {
+		return &Requester{
+			header: make(http.Header),
+		}
+	},
 }
 
 func NewRequester() *Requester {
-	return &Requester{
-		header: map[string][]string{},
-	}
+	return requesterPool.Get().(*Requester).Clear()
 }
 
 func (h *Requester) Get(u string) *Requester {
 	h.url = u
 	h.m = http.MethodGet
+	return h
+}
+
+func (h *Requester) RequestJSON(method string, u string, obj any) *Requester {
+	h.m = method
+	h.url = u
+	h.SetHead("content-type", "application/json")
+	if obj != nil {
+		var buf bytes.ByteBuffer
+		h.err = json.NewEncoder(&buf).Encode(obj)
+	}
 	return h
 }
 
@@ -34,6 +55,10 @@ func (h *Requester) Post(u string, body io.Reader) *Requester {
 	return h
 }
 
+func (h *Requester) PostJSON(u string, obj any) *Requester {
+	return h.RequestJSON(http.MethodPost, u, obj)
+}
+
 func (h *Requester) Put(u string, body io.Reader) *Requester {
 	h.m = http.MethodPut
 	h.url = u
@@ -41,11 +66,19 @@ func (h *Requester) Put(u string, body io.Reader) *Requester {
 	return h
 }
 
+func (h *Requester) PutJSON(u string, obj any) *Requester {
+	return h.RequestJSON(http.MethodPut, u, obj)
+}
+
 func (h *Requester) Delete(u string, body io.Reader) *Requester {
 	h.m = http.MethodDelete
 	h.url = u
 	h.body = body
 	return h
+}
+
+func (h *Requester) DeleteJSON(u string, obj any) *Requester {
+	return h.RequestJSON(http.MethodDelete, u, obj)
 }
 
 func (h *Requester) AddHead(key, value string) *Requester {
@@ -59,6 +92,9 @@ func (h *Requester) SetHead(key, value string) *Requester {
 }
 
 func (h *Requester) Response(ctx context.Context) (*http.Response, error) {
+	if h.err != nil {
+		return nil, h.err
+	}
 	req, err := http.NewRequestWithContext(ctx, h.m, h.url, h.body)
 	if err != nil {
 		return nil, err
@@ -79,4 +115,37 @@ func (h *Requester) AsBytes(ctx context.Context) []byte {
 
 func (h *Requester) AsString(ctx context.Context) string {
 	return bytes.ToString(h.AsBytes(ctx))
+}
+
+func (h *Requester) AsJSON(ctx context.Context, obj any) error {
+	resp, err := h.Response(ctx)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(obj)
+}
+
+func (h *Requester) Clear() *Requester {
+	if len(h.header) > 0 {
+		h.header = make(http.Header)
+	}
+	h.err = nil
+	h.url = ""
+	h.m = ""
+	h.body = nil
+	return h
+}
+
+func (h *Requester) Clone() *Requester {
+	new := NewRequester()
+	new.header = h.header.Clone()
+	new.url = h.url
+	new.m = h.m
+	new.body = h.body
+	return new
+}
+
+func (h *Requester) Close() {
+	requesterPool.Put(h)
 }
