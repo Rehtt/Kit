3a48a00fd1ea4afaa7bc6e57d6687dacd4f32111
Rehtt <dsreshiram@gmail.com>
2025-05-26 15:30:50 +0800

    :sparkles: Try add Finally

--- util/try.go
+++ util/try.go
@@ -1,11 +1,22 @@
 package util
 
+import "sync"
+
 type try struct {
 	err any
 }
 
+var tryPool = sync.Pool{
+	New: func() any { return new(try) },
+}
+
 func Try(fn func()) (t *try) {
-	t = new(try)
+	t = tryPool.Get().(*try)
+	t.err = nil
+	if fn == nil {
+		return nil
+	}
+
 	defer func() {
 		if err := recover(); err != nil {
 			t.err = err
@@ -14,6 +25,16 @@ func Try(fn func()) (t *try) {
 	fn()
 	return &try{}
 }
+
 func (t *try) Catch(fn func(err any)) {
-	fn(t.err)
+	if fn != nil {
+		fn(t.err)
+	}
+}
+
+func (t *try) Finally(fn func()) {
+	if fn != nil {
+		fn()
+	}
+	tryPool.Put(t)
 }
