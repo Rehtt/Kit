583f463a29392d89d1a34084227da34ca2051031
Rehtt <dsreshiram@gmail.com>
2025-10-14 17:58:18 +0800

    docs: 为 PasswordStringVar 和 PasswordString 函数添加注释说明

--- cli/flag.go
+++ cli/flag.go
@@ -6,6 +6,9 @@ type FlagSet struct {
 	*flag.FlagSet
 }
 
+// PasswordStringVar 定义一个密码字符串类型 flag（使用默认 CommandLine 实例）
+// 在帮助信息中，密码值会显示为 ********
+// showNum 指定密码显示的字符个数
 func (f *FlagSet) PasswordStringVar(p *string, name string, value string, usage string, showNum ...int) {
 	*p = value
 	var num int
@@ -15,6 +18,9 @@ func (f *FlagSet) PasswordStringVar(p *string, name string, value string, usage 
 	f.Var(&passwordValue{value: p, show: num}, name, usage)
 }
 
+// PasswordString 定义并返回一个密码字符串类型 flag 指针（使用默认 CommandLine 实例）
+// 在帮助信息中，密码值会显示为 ********
+// showNum 指定密码显示的字符个数
 func (f *FlagSet) PasswordString(name string, value string, usage string, showNum ...int) *string {
 	p := new(string)
 	f.PasswordStringVar(p, name, value, usage, showNum...)
