6f67e11b9625413ddd6d7c204c48ca0c84a02c08
Rehtt <dsreshiram@gmail.com>
2025-12-11 17:16:29 +0800

    refactor: 使用泛型重写切片分割函数并添加迭代方法

--- slice/split.go
+++ slice/split.go
@@ -1,34 +1,44 @@
 package slice
 
-import (
-	"reflect"
-)
-
 // Split 将一个切片划分为多个大小为len的切片
-func Split(data any, len int) (out []any) {
-	if len < 1 {
-		return
+func Split[Slice ~[]E, E any](data Slice, n int) (out []Slice) {
+	if n < 1 {
+		return nil
 	}
-	ty := reflect.ValueOf(data)
-	if ty.Kind() == reflect.Pointer {
-		ty = ty.Elem()
-	}
-	l := len
-	for i := 0; i < ty.Len(); {
-		if i+len >= ty.Len() {
-			len = ty.Len() - i
-		}
-		var da reflect.Value
-		if reflect.TypeOf(data).Kind() == reflect.Pointer {
-			da = reflect.New(ty.Slice(i, i+len).Type())
-			da.Elem().Set(ty.Slice(i, i+len))
-		} else {
-			da = reflect.ValueOf(ty.Slice(i, i+len).Interface())
-		}
-		i += len
-		if i%l == 0 || l != len {
-			out = append(out, da.Interface())
-		}
+	out = make([]Slice, 0, len(data)/n+1)
+	for i := 0; i < len(data); i += n {
+		end := min(n, len(data[i:]))
+		out = append(out, data[i:i+end:i+end])
 	}
 	return
+}
+
+// IterSplit 将一个切片划分为多个大小为len的切片
+// 并且通过一个函数迭代切片
+func IterSplit[Slice ~[]E, E any](data Slice, n int) func(yield func(Slice) bool) {
+	return func(yield func(Slice) bool) {
+		if n < 1 {
+			return
+		}
+		for i := 0; i < len(data); i += n {
+			end := min(n, len(data[i:]))
+			if !yield(data[i : i+end : i+end]) {
+				return
+			}
+		}
+	}
+}
+
+func IterSplit2[Slice ~[]E, E any](data Slice, n int) func(yield func(Slice, int) bool) {
+	return func(yield func(Slice, int) bool) {
+		if n < 1 {
+			return
+		}
+		for i := 0; i < len(data); i += n {
+			end := min(n, len(data[i:]))
+			if !yield(data[i:i+end:i+end], i) {
+				return
+			}
+		}
+	}
 }
--- slice/split_test.go
+++ slice/split_test.go
@@ -1,26 +1,188 @@
 package slice
 
 import (
-	"fmt"
+	"slices"
 	"testing"
 )
 
+func cmpSlice[E comparable](a, b [][]E) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if len(a[i]) != len(b[i]) {
+			return false
+		}
+
+		if !slices.Equal(a[i], b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
 func TestSplit(t *testing.T) {
-	a := []int{1, 2, 3}
-	for _, v := range Split(a, 2) {
-		fmt.Println(v.([]int))
+	type args struct {
+		data []int
+		n    int
 	}
-	for _, v := range Split(&a, 2) {
-		fmt.Println(v.(*[]int))
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test1",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    3,
+			},
+			want: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10}},
+		},
+		{
+			name: "test2",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    2,
+			},
+			want: [][]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}},
+		},
+		{
+			name: "test3",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    1,
+			},
+			want: [][]int{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}},
+		},
+		{
+			name: "test4",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    0,
+			},
+			want: nil,
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Split(tt.args.data, tt.args.n); !cmpSlice(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	b := []string{"a", "b", "c"}
-	for _, v := range Split(b, 2) {
-		fmt.Println(v.([]string))
+func TestIterSplit(t *testing.T) {
+	type args struct {
+		data []int
+		n    int
 	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test1",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    3,
+			},
+			want: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10}},
+		},
+		{
+			name: "test2",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    2,
+			},
+			want: [][]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}},
+		},
+		{
+			name: "test3",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    1,
+			},
+			want: [][]int{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}},
+		},
+		{
+			name: "test4",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    0,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got [][]int
+			IterSplit(tt.args.data, tt.args.n)(func(v []int) bool {
+				got = append(got, v)
+				return true
+			})
+			if !cmpSlice(got, tt.want) {
+				t.Errorf("IterSplit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	c := []float64{1.234, 3.2345, 5.123}
-	for _, v := range Split(c, 2) {
-		fmt.Println(v.([]float64))
+func TestIterSplit2(t *testing.T) {
+	type args struct {
+		data []int
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test1",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    3,
+			},
+			want: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10}},
+		},
+		{
+			name: "test2",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    2,
+			},
+			want: [][]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}},
+		},
+		{
+			name: "test3",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    1,
+			},
+			want: [][]int{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}},
+		},
+		{
+			name: "test4",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n:    0,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got [][]int
+			IterSplit2(tt.args.data, tt.args.n)(func(v []int, i int) bool {
+				got = append(got, v)
+				return true
+			})
+			if !cmpSlice(got, tt.want) {
+				t.Errorf("IterSplit2() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
