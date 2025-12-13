d05780011e054fff55ceb1d4ce970dc6c21b0db7
Rehtt <dsreshiram@gmail.com>
2025-12-13 19:02:31 +0800

    feat: 增强 JSON 类型的扫描和序列化功能，支持空值和自定义类型转换

--- db/json.go
+++ db/json.go
@@ -15,19 +15,31 @@ func (JSON[T]) GormDataType() string {
 }
 
 func (j *JSON[T]) Scan(value any) error {
+	if value == nil {
+		return nil
+	}
+
 	var data []byte
 	switch v := value.(type) {
 	case []byte:
 		data = v
 	case string:
 		data = []byte(v)
+	default:
+		b, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		data = b
+	}
+	if len(data) == 0 {
+		return nil
 	}
 	return json.Unmarshal(data, &j.data)
 }
 
 func (j JSON[T]) Value() (value driver.Value, err error) {
-	src, err := json.Marshal(j.data)
-	return src, err
+	return json.Marshal(j.data)
 }
 
 func (j JSON[T]) Get() T {
@@ -36,4 +48,24 @@ func (j JSON[T]) Get() T {
 
 func (j *JSON[T]) Set(data T) {
 	j.data = data
+}
+
+func (j JSON[T]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(j.data)
+}
+
+func (j *JSON[T]) UnmarshalJSON(b []byte) error {
+	return json.Unmarshal(b, &j.data)
+}
+
+func (j JSON[T]) Convert() any {
+	return j.data
+}
+
+func (j *JSON[T]) UnmarshalValue(value any) error {
+	return j.Scan(value)
+}
+
+func NewJSON[T any](data T) JSON[T] {
+	return JSON[T]{data}
 }
