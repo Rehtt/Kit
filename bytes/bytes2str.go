bba7ce9b4c8f4a774aabd0970f23a98d84729ab1
Rehtt <dsreshiram@gmail.com>
2025-12-22 09:30:24 +0800

    refactor: 弃用旧的高性能转换函数，改用 unsafe 包的新方法

--- bytes/bytes2str.go
+++ bytes/bytes2str.go
@@ -3,8 +3,17 @@ package bytes
 import "unsafe"
 
 // UnsafeToString 高性能转换
+// Deprecated: 请使用 UnsafeBytesToString
+// 有 GC 风险，uintptr是整数可能导致 GC 不知道这是指针，最后 GC 回收了底层数组
 func UnsafeToString(b []byte) string {
 	x := (*[3]uintptr)(unsafe.Pointer(&b))
 	h := [2]uintptr{x[0], x[1]}
 	return *(*string)(unsafe.Pointer(&h))
+}
+
+func UnsafeBytesToString(b []byte) string {
+	if len(b) == 0 {
+		return ""
+	}
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
--- strings/string2bytes.go
+++ strings/string2bytes.go
@@ -3,8 +3,17 @@ package strings
 import "unsafe"
 
 // UnsafeToBytes 高性能转换
+// Deprecated: 请使用 UnsafeStringToBytes
+// 有 GC 风险，uintptr是整数可能导致 GC 不知道这是指针，最后 GC 回收了底层数组
 func UnsafeToBytes(s string) []byte {
 	x := (*[2]uintptr)(unsafe.Pointer(&s))
 	h := [3]uintptr{x[0], x[1], x[1]}
 	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func UnsafeStringToBytes(str string) []byte {
+	if str == "" {
+		return nil
+	}
+	return unsafe.Slice(unsafe.StringData(str), len(str))
 }
